f7742bf39420c44ab87029cafd160b4c88d342f0
Almognach <73398227+Almognach@users.noreply.github.com>
2020-11-05 23:13:09 +0200

    Populated Models
    Signed-off-by: Almognach <73398227+Almognach@users.noreply.github.com>

--- Electro-Shop/Electro-Shop/Models/Category.cs
+++ Electro-Shop/Electro-Shop/Models/Category.cs
@@ -7,5 +7,9 @@ namespace Electro_Shop.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category Parent { get; set; }
+
     }
 }
--- Electro-Shop/Electro-Shop/Models/Product.cs
+++ Electro-Shop/Electro-Shop/Models/Product.cs
@@ -7,5 +7,14 @@ namespace Electro_Shop.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public uint SalesCounter { get; set; }
+        public uint InStock { get; set; }
+        public Uri PathToImage { get; set; }
+
     }
 }
