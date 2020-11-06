affa8542095df28da8ac5cecdf2a72fb19f3fc67
Almognach <73398227+Almognach@users.noreply.github.com>
2020-11-06 12:15:06 +0200

    Added some untracked fileד
    Signed-off-by: Almognach <73398227+Almognach@users.noreply.github.com>

---
+++ Electro-Shop/Electro-Shop/Migrations/Product/20201105213640_Product.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Electro_Shop.Migrations.Product
+{
+    public partial class Product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: true),
+                    SalesCounter = table.Column<long>(nullable: false),
+                    InStock = table.Column<long>(nullable: false),
+                    PathToImage = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+        }
+    }
+}
