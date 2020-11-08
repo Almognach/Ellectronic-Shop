341acb09051a910703091724d7fdf02a36d0a407
almogsnir <73750682+almogsnir@users.noreply.github.com>
2020-11-08 19:45:37 +0200

    Best sellers

---
+++ Electro-Shop/Electro-Shop/Controllers/ContactUsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Electro_Shop.Data;
+using Electro_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Electro_Shop.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private readonly ILogger<ContactUsController> _logger;
+        private readonly ContactUsContext _context;
+
+
+        public ContactUsController(ILogger<ContactUsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("FirstName,LastName,Phone,Email,IssueWith")] ContactUsSubmit contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+    }
+}
