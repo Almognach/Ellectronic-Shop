5e9ba91989fae3fc469c501c47f27184cb4255dc
almogsnir <73750682+almogsnir@users.noreply.github.com>
2020-11-15 23:35:41 +0200

    Added search logic + controller + view

---
+++ Electro-Shop/Electro-Shop/Models/Search.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Electro_Shop.Areas.Identity.Data;
+
+namespace Electro_Shop.Models
+{
+    public class Search
+    {
+        [Key]
+        public string KeyID { get; set; }
+    }
+}
